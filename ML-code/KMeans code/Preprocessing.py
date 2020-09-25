7ff87d7f442acbf46b1d695f9c391f535c5b15b3
raulroy45 <raul.roy45@gmail.com>
2020-09-26 03:29:35 +0530

    Major changes to software code.

--- ML-code/KMeans code/Preprocessing.py
+++ ML-code/KMeans code/Preprocessing.py
@@ -1,7 +1,29 @@
+# TODO Figure out whether how to make df_artists based on SpotifyRecommender.
+
 # Preprocesses all the necessary data required.
+import pickle
+import pandas as pd
+import numpy as np
 from csv import reader
 from ast import literal_eval
-import pickle
+from sklearn.cluster import MiniBatchKMeans
+from sklearn.preprocessing import StandardScaler
+
+# Loading the Spotify song data and then selecting the useful features
+features = ['acousticness', 'danceability', 'energy', 'instrumentalness', 'liveness', 'loudness', 'speechiness', 'tempo', 'valence']
+df_full = pd.read_csv("spotify-web-app\ML-code\Data\data.csv")
+df_full = df_full[(df_full.popularity > 30) & (df_full.year > 1980)]
+df = df_full[features]
+df_artists = pd.read_csv("ML-code/Data/data_by_artist.csv")
+df_artists = df_artists[features]
+
+# Standarizing all column values
+scaler = StandardScaler().fit(df.values)
+X_std = scaler.transform(df.values)
+
+# MiniBatchKMeans model formation fitted according to standardized song feature values.
+kmeans = MiniBatchKMeans(n_clusters=50, random_state=0, batch_size=100)
+kmeans = kmeans.fit(X_std)
 
 artists_by_genres = {}
 with open('ML-code/Data/data_by_genres.csv', 'r') as read_obj0:
@@ -13,9 +35,19 @@ with open('ML-code/Data/data_w_genres.csv', 'r') as read_obj1:
     csv_reader = reader(read_obj1)
     next(csv_reader, None)
     for row in csv_reader:
-        artist = row[0]
+        artist_attributes = []
+        for index in range(1, 11):
+            if index == 3:
+                continue
+            artist_attributes.append(row[index])
+        artist = (row[0], artist_attributes)
         genres = literal_eval(row[15])
         for genre in genres:
             artists_by_genres[genre].append(artist)
 
+pickle.dump(scaler, open("spotify-web-app\ML-code\KMeans code\pickled_data\scaler.pkl", "wb"))
+pickle.dump(kmeans, open("spotify-web-app\ML-code\KMeans code\pickled_data\model.pkl", "wb"))
 pickle.dump(artists_by_genres, open('ML-code/KMeans code/pickled_data/artists_by_genre.pkl', 'wb'))
+df_artists.to_csv("spotify-web-app\ML-code\KMeans code\KMeans_data\cleaned_artist_data.csv")
+df.to_csv("spotify-web-app\ML-code\KMeans code\KMeans_data\cleaned_feature_data.csv", index=False)
+df_full.to_csv("spotify-web-app\ML-code\KMeans code\KMeans_data\cleaned_data.csv", index=False)
---
+++ ML-code/KMeans code/SpotifyRecommender.py
@@ -0,0 +1,86 @@
+import pandas as pd
+import numpy as np
+import pickle
+from scipy.spatial import distance
+from sklearn.preprocessing import StandardScaler
+
+class SpotifyRecommender:
+    def __init__(self):
+        self.df = (pd.read_csv("ML-code/KMeans code/KMeans_data/cleaned_feature_data.csv")).to_numpy()
+        # can remove this after adjusting according to the specific API calls.
+        self.data_full = (pd.read_csv("ML-code/KMeans code/KMeans_data/cleaned_data.csv"))
+        self.df_artists = pd.read_csv("ML-code/Data/data_w_genres.csv", header=False)
+        self.kmeans = pickle.load(open("ML-code/KMeans code/pickled_data/model.pkl", "rb"))
+        self.scaler = pickle.load(open("ML-code/KMeans code/pickled_data/scaler.pkl", "rb"))
+        self.artist_genres = pickle.load(open("ML-code/KMeans code/pickled_data/artists_by_genre.pkl", "rb"))
+    
+    def recommend(self, artist_name, spotifyQuery):
+        # Preparing all recommendations.
+        recommend_list = []
+        recommend_list.extend(self.__artist_rec(artist_name, spotifyQuery))
+        recommend_list.extend(self.__genre_rec(artist_name))
+        recommend_list.extend(self.__ML_rec(spotifyQuery))
+        
+        # Returning printable values (CHANGE THIS ACCORDING TO YOUR API NEEDS)
+        ret = []
+        for index in range(len(recommend_list)):
+            i = (recommend_list[index])[0]
+            ret.append(data_full._get_value(i, "name") + " by " +\
+                       data_full._get_value(i, "artists"))
+        return ret
+
+    # TODO: Make /cleaned_artist.csv to get artist_attributes
+    def __genre_rec(self, artist):
+        # Getting the artist's information.
+        artist_genres = (self.df_artists.loc[self.df_artists['artists'] == artist]).iloc[0][15]
+        artist_attribute = None # TODO (in nd_array)
+        # Getting similar artists based on common genres.
+        common_artists = {}
+        for genre in artist_genres:
+            genre_list = artist_genres.get(genre)
+            for other_artist in genre_list:
+                if not other_artist in common_artists:
+                    common_artists[other_artist] = 1
+            else:
+                common_artists[other_artist] += 1
+        sorted_list = sorted(common_artists.keys(), key=lambda x: x[1])
+
+        # Only using the most common artists.
+        highest_value = common_artists.get(sorted_list[0])
+        df_common_artists = self.df_artists.loc[sorted_list[0]]
+        for i in range(1, len(sorted_list)):
+            if common_artists.get(sorted_list[i]) < highest_value:
+                break
+            df_common_artists.append(self.df_artists.loc[sorted_list[i]]) 
+        
+        return self.__euclidean_min(df_common_artists, artist_attribute, \
+                                    [x for x in range(0, df_common_artists.count())], output_num=2)
+            
+
+    def __artist_rec(self, artist, spotifyQuery):
+        df_artist_songs = (self.df_artists.loc[df_artists['artists'] == artist])
+        return self.__euclidean_min(df_artist_songs, spotifyQuery.to_numpy(),\
+                                    [x for x in range(0, df_artist_songs.count())], output_num=2)
+
+    def __ML_rec(self, spotifyQuery):
+        # standardizing the query song and getting its cluster label.
+        query_std = self.scaler.transform([spotifyQuery])
+        cluster_label = self.kmeans.predict(query_std)
+
+        # Getting all the cluster points from the query song's cluster label.
+        labels = self.kmeans.labels_
+        indexes = []
+        for index in range(len(labels)):
+            if (labels[index] == cluster_label):
+                indexes.append(index)
+        
+        return self.__euclidean_min(self.df, query_std, indexes)
+
+    # Calculating normal euclidean distance between desired mutli-dimensional points and returning
+    # only the closest desired number of points.
+    def __euclidean_min(self, _df, data_point, indexes, output_num=5):
+        distances = {}
+        for index in indexes:
+            point = tuple(self.scaler.transform([_df[index]]))
+            distances[index] = distance.euclidean(point, tuple(data_point))
+        return (sorted(distances.items(), key=lambda x: x[1]))[:output_num]
