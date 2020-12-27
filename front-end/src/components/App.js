7e8cc3565c6a591bfbf0c406f220a1763a9dd3aa
Rishabh Goyal <rgoyal17@uw.edu>
2020-12-26 17:35:27 -0800

    login updates

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -3,6 +3,21 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import HomePage from './HomePage';
 import ExplorePage from './ExplorePage'
 import QuizPage from './QuizPage'
+// const SpotifyWebApi = require('spotify-web-api-node');
+// const spotifyApi = new SpotifyWebApi({
+//   clientId: 'f2d6728148db4fdaae6137fb108b8724',
+//   clientSecret: 'a8fecbb751c94c5a953e852504101688',
+//   redirectUri: 'http://localhost:3000/'
+// });
+
+// spotifyApi.setAccessToken('BQAsSmtN4sx5rxi4Gx9F1n1foD8h-AOVP7ywwAAflt5dXJFCs9UJDoSv6bvr7OxlCQdjsZJbU5_npe8bGvLcYDyDyZvuw4-AYObcVU9zXFcnBKsDGyj-YaUEp8063EK07YrP-522-98G7g6jszNXGE_pQ9KS_CCo3k1thiX4S6Wr0lhYWCqjnd_OsA');
+
+// spotifyApi.searchTracks('Love')
+//   .then(function(data) {
+//     console.log('Search by "Love"', data.body);
+//   }, function(err) {
+//     console.error(err);
+//   });
 
 class App extends React.Component {
   constructor(props) {
